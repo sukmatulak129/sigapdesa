dfc803e00663e4e2c21317f4a2bd680c46272a7e
Sukma Novianti Tulak <sukmatulak@gmail.com>
2026-06-19 10:48:25 +0700

    Add files via upload

---
+++ bootstrap/app.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        // Tambahkan middleware admin di sini
+        $middleware->alias([
+            'admin' => App\Http\Middleware\AdminMiddleware::class,
+        ]);
+        
+        // Jika perlu menambahkan middleware ke group, bisa dilakukan di sini
+        // $middleware->appendToGroup('web', [
+        //     // middleware tambahan
+        // ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
